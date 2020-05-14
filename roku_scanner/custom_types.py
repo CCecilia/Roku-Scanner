25b0cf69a20a46ddb51327deb48d901285ae016e
christian cecilia <christian.cecilia1@gmail.com>
2020-05-13 20:56:07 -0400

    Added docstrings to __main__.py, custom_types.py, and scanner.py. Updated discovery data type.

--- roku_scanner/custom_types.py
+++ roku_scanner/custom_types.py
@@ -2,14 +2,26 @@ import argparse
 import asyncio
 import socket
 from pathlib import Path
-from typing import Dict
+from typing import Dict, Union
 
 import requests
 
+Address = Dict[str, str]
 ArgList = argparse.Namespace
 ArgParser = argparse.ArgumentParser
 SocketConnection = socket.socket
-DiscoveryData = Dict[str, any]
+"""
+DiscoveryData:
+    Cache-Control: str
+    ST: str
+    USN: str
+    Ext: str
+    Server: str
+    LOCATION: str
+    device-group.roku.com: str
+    WAKEUP: str
+"""
+DiscoveryData = Dict[str, Union[str, Address]]
 Response = requests.models.Response
 Task = asyncio.Task
 PathType = Path
